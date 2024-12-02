eb3bf5d812a543bd922b3982c3318544e595efe2
RuchiraKarunatissa <ruchira.karunatissa@gmail.com>
2024-12-02 11:24:29 +0530

    Fix unused imports in App.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,12 @@
-import React, { useState } from 'react';
-import { Search, Calendar, Clock, ArrowLeft, HelpCircle, AlertCircle, Check } from 'lucide-react';
+import React from 'react';
+import AppointmentSystem from './components/AppointmentSystem';
 
-// UI Components
-const Card = React.forwardRef(({ className = '', ...props }, ref) => (
-  <div
-    ref={ref}
-    className={`rounded-lg border bg-white shadow-sm ${className}`}
-    {...props}
-  />
-));
-
-const CardContent = React.forwardRef(({ className = '', ...props }, ref) => (
-  <div ref={ref} className={`p-6 ${className}`} {...props} />
-));
-
-const Alert = React.forwardRef(({ className = '', ...props }, ref) => (
-  <div
-    ref={ref}
-    role="alert"
-    className={`relative w-full rounded-lg border p-4 ${className}`}
-    {...props}
-  />
-));
-
-const AlertDescription = React.forwardRef(({ className = '', ...props }, ref) => (
-  <div
-    ref={ref}
-    className={`text-sm ${className}`}
-    {...props}
-  />
-));
-
-// Your existing AppointmentSystem code goes here (everything else stays exactly the same)
-const App = () => {
-  // Copy all the code from your current AppointmentSystem.jsx here,
-  // starting from the specializations constant through to the end
-  // Just replace 'export default AppointmentSystem' with 'export default App'
-};
+function App() {
+  return (
+    <div className="App">
+      <AppointmentSystem />
+    </div>
+  );
+}
 
 export default App;
